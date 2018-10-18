65de9ef7474651b72bfdc516aae6fa86bbf9f2e6
kosgiwx <44200302+kosgiwx@users.noreply.github.com>
2018-10-18 12:58:48 +0900

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,7 +4,8 @@ module.exports.run = async (bot, message, args) => {
 if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
    {
       return message.reply("당신의 권한 대체되었다. yourmom");
-}
+}else
+{
 module.exports.run = async (bot, message, args) => {
     message.channel.send(`!ban 밴할놈 사유`);
     message.channel.send(`!kick 킥할놈 사유`);
@@ -13,6 +14,7 @@ module.exports.run = async (bot, message, args) => {
     message.channel.send(`!unmute 채팅,보이스 불가 "사면" 시킬놈`);
 
 };
+}
 }
 
 module.exports.help = {
