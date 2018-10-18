76de89f1ef4831408efcbce20e9d2e5840ac4211
kosgiwx <44200302+kosgiwx@users.noreply.github.com>
2018-10-18 13:11:56 +0900

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,9 +1,10 @@
 const discord = require("discord.js");
 const config = require('../config.json');
 module.exports.run = async (bot, message, args) => {
-if(message.author.hasPermission('MANAGE_CHANNELS'){
+if(message.author.hasPermission('MANAGE_CHANNELS'))
+      {
       return message.reply("당신의 권한 대체되었다. yourmom");
-}
+      }
    
    if(message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) ) {
 module.exports.run = async (bot, message, args) => {
@@ -13,7 +14,8 @@ module.exports.run = async (bot, message, args) => {
     message.channel.send(`!mute 채팅,보이스 불가 시킬놈 시간 사유`);
     message.channel.send(`!unmute 채팅,보이스 불가 "사면" 시킬놈`);
 
-};
+            };
+   }
 }
 
 
