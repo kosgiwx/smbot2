efac25f53a3a9ff59f367fbd6a6c837bf297f174
kosgiwx <44200302+kosgiwx@users.noreply.github.com>
2018-10-18 13:17:14 +0900

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,12 +1,19 @@
 const discord = require("discord.js");
 const config = require('../config.json');
 module.exports.run = async (bot, message, args) => {
-if(message.author.hasPermission('MANAGE_CHANNELS'))
+      
+let ADMIN = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+if(ADMIN.hasPermission('MANAGE_CHANNELS'))
       {
       return message.reply("당신의 권한 대체되었다. yourmom");
       }
+      
+if(!message.member.hasPermission('MANAGE_CHANNELS'))
+      {
+      return message.reply("당신의 권한 애초에 없었다. yourmom");
+      }
    
-   if(message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) ) {
+if(message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) ) {
 module.exports.run = async (bot, message, args) => {
     message.channel.send(`!ban 밴할놈 사유`);
     message.channel.send(`!kick 킥할놈 사유`);
